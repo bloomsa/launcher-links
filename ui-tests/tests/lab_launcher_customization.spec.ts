1de50771974aa21eede250c349d67c1f632e6ee2
sambloomquist <bloomquist.sam@gmail.com>
2025-09-19 18:31:59 -0500

    cleanup after rename

--- ui-tests/tests/lab_launcher_customization.spec.ts
+++ ui-tests/tests/lab_launcher_customization.spec.ts
@@ -16,6 +16,6 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension lab-launcher-customization is activated!')
+    logs.filter(s => s === 'JupyterLab extension launcher-links is activated!')
   ).toHaveLength(1);
 });
